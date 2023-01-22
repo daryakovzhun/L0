ea0645e18032331de5e723280bc823d3fb8d2308
Daria Kovzhun <dariakovzhun@gmail.com>
2023-01-22 18:54:18 +0300

    router index and order html

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,7 +3,7 @@ package handler
 import (
 	"L0WB/pkg/service"
 	"encoding/json"
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -17,17 +17,18 @@ func NewHandler(services *service.Service) *Handler {
 }
 
 func (h *Handler) GetOrderById(w http.ResponseWriter, r *http.Request) {
+	temp, _ := template.ParseFiles("web/order.html")
 	uid := r.FormValue("uid")
 
 	ord, _ := h.services.Cache[uid]
 	data, _ := json.MarshalIndent(ord, "", "\t")
-	fmt.Fprintf(w, "%s", data)
+	temp.Execute(w, string(data))
 
 }
 
 func (h *Handler) InitRouters() {
-	http.HandleFunc("/order", h.GetOrderById)
 	http.Handle("/", http.FileServer(http.Dir("web/")))
+	http.HandleFunc("/order", h.GetOrderById)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
